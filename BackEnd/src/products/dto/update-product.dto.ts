b731d27fd9eaa1566377c7f697efe04e5821aafa
log1ri <feldekper@gmail.com>
2025-06-19 15:47:41 +0700

    add detail at dto update

--- BackEnd/src/products/dto/update-product.dto.ts
+++ BackEnd/src/products/dto/update-product.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+    @IsOptional()
+    title?: string;
+
+    @IsOptional()
+    completed?: boolean;
+}
