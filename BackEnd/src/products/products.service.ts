316dd52a4e96e6eff502b9c803d1467d64febe3c
log1ri <feldekper@gmail.com>
2025-06-19 16:05:18 +0700

    add import, todos var and find all at service

--- BackEnd/src/products/products.service.ts
+++ BackEnd/src/products/products.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './entities/product.entity';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class ProductsService {
+
+  private todos: Product[] = [{"id":"1","title":"test","completed":false},{"id":"2","title":"tes2","completed":true}];
+
+  findAll() {
+    return this.todos;
+  }
+
+
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
 
-  findAll() {
-    return `This action returns all products`;
-  }
 
   findOne(id: number) {
     return `This action returns a #${id} product`;
