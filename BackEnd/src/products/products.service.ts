1df4db361f9bbdc0733b51bd906d716513c4bcd5
log1ri <feldekper@gmail.com>
2025-06-19 16:33:25 +0700

    fix type at findone, add findOne and add output when find succese

--- BackEnd/src/products/products.service.ts
+++ BackEnd/src/products/products.service.ts
@@ -9,19 +9,30 @@ export class ProductsService {
 
   private todos: Product[] = [{"id":"1","title":"test","completed":false},{"id":"2","title":"tes2","completed":true}];
 
-  findAll() {
-    return this.todos;
-  }
-
-
+  
+  
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
-
-
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  
+  findAll() {
+    console.log('Fetching all todos');
+    if (this.todos.length === 0) {
+      return [];
+    }
+    return this.todos;
   }
+
+  findOne(id: string): Product| undefined {
+    const foundId = this.todos.find(todo => todo.id === id);
+    if (!foundId) {
+      return undefined;
+    }
+    console.log('Found todo with id:', foundId.id);
+    return foundId;
+
+  }
+
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
