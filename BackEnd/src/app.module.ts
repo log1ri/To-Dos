faed1baa5d18ea6fb5ba236eab19bfcc3a624380
Thunyathep <feldekper@gmail.com>
2025-06-21 00:08:59 +0700

    import mongoose at app/product.module and schemas

--- BackEnd/src/app.module.ts
+++ BackEnd/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot('mongodb://localhost/nest') // Replace with your MongoDB connection string
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- BackEnd/src/products/products.module.ts
+++ BackEnd/src/products/products.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 
