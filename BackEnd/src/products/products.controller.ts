9adeb4f5ddb1bb48278c964a46967efbacbf2f69
log1ri <feldekper@gmail.com>
2025-06-20 16:30:01 +0700

    refac create findall control

--- BackEnd/src/products/products.controller.ts
+++ BackEnd/src/products/products.controller.ts
@@ -1,20 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Response } from 'express';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  create(@Body() dto: CreateProductDto, @Res() res: Response) {
+    const product = this.productsService.create(dto);
+    if (!product) {
+      return res.status(400).json({
+        message: 'Error creating product',
+      });
+    }
+    return res.status(201).json({
+      message: 'Todo created successfully',
+      data: product
+    });
   }
 
   @Get()
-  findAll() {
-    return this.productsService.findAll();
+  findAll(@Res() res: Response) {
+    const product = this.productsService.findAll();
+    return res.status(200).json({
+      message: 'Todos fetched successfully',
+      data: product
+    });
   }
 
   @Get(':id')
